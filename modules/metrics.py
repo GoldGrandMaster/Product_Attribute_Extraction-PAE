4c3bd4367e93df65e33c59aaf9adf6c547790713
passioneffort <agronkercishta.ai@gmail.com>
2023-10-01 06:34:43 -0400

    modify

--- modules/metrics.py
+++ modules/metrics.py
@@ -13,15 +13,15 @@ def emotion_detection_metrics_fn(list_hyp, list_label):
     return metrics
 
 
-# def aspect_extraction_metrics_fn(list_hyp, list_label):
-#     metrics = {}
-#     acc, pre, rec, f1, tm_pre, tm_rec, tm_f1 = conll_evaluation(
-#         list_hyp, list_label)
-#     metrics["ACC"] = acc
-#     metrics["F1"] = tm_f1
-#     metrics["REC"] = tm_rec
-#     metrics["PRE"] = tm_pre
-#     return metrics
+def aspect_extraction_metrics_fn(list_hyp, list_label):
+    metrics = {}
+    acc, pre, rec, f1, tm_pre, tm_rec, tm_f1 = conll_evaluation(
+        list_hyp, list_label)
+    metrics["ACC"] = acc
+    metrics["F1"] = tm_f1
+    metrics["REC"] = tm_rec
+    metrics["PRE"] = tm_pre
+    return metrics
 
 
 def ner_metrics_fn(list_hyp, list_label):
