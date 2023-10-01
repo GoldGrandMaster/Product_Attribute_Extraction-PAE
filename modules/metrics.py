4c9ae6523ddc699d8e99be7bf8eeeec97f8726bf
passioneffort <agronkercishta.ai@gmail.com>
2023-10-01 06:34:29 -0400

    modify

--- modules/metrics.py
+++ modules/metrics.py
@@ -4,13 +4,13 @@ from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_sc
 from .conlleval import conll_evaluation
 
 
-# def emotion_detection_metrics_fn(list_hyp, list_label):
-#     metrics = {}
-#     metrics["ACC"] = accuracy_score(list_label, list_hyp)
-#     metrics["F1"] = f1_score(list_label, list_hyp, average='macro')
-#     metrics["REC"] = recall_score(list_label, list_hyp, average='macro')
-#     metrics["PRE"] = precision_score(list_label, list_hyp, average='macro')
-#     return metrics
+def emotion_detection_metrics_fn(list_hyp, list_label):
+    metrics = {}
+    metrics["ACC"] = accuracy_score(list_label, list_hyp)
+    metrics["F1"] = f1_score(list_label, list_hyp, average='macro')
+    metrics["REC"] = recall_score(list_label, list_hyp, average='macro')
+    metrics["PRE"] = precision_score(list_label, list_hyp, average='macro')
+    return metrics
 
 
 def aspect_extraction_metrics_fn(list_hyp, list_label):
