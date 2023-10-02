7dbc16ad7d2e04c666062f515d5eca0e840078f6
passioneffort <agronkercishta.ai@gmail.com>
2023-10-02 11:48:15 -0400

    modify

--- modules/metrics.py
+++ modules/metrics.py
@@ -75,15 +75,15 @@ def keyword_extraction_metrics_fn(list_hyp, list_label):
     return metrics
 
 
-def qa_factoid_metrics_fn(list_hyp, list_label):
-    metrics = {}
-    acc, pre, rec, f1, tm_pre, tm_rec, tm_f1 = conll_evaluation(
-        list_hyp, list_label)
-    metrics["ACC"] = acc
-    metrics["F1"] = tm_f1
-    metrics["REC"] = tm_rec
-    metrics["PRE"] = tm_pre
-    return metrics
+# def qa_factoid_metrics_fn(list_hyp, list_label):
+#     metrics = {}
+#     acc, pre, rec, f1, tm_pre, tm_rec, tm_f1 = conll_evaluation(
+#         list_hyp, list_label)
+#     metrics["ACC"] = acc
+#     metrics["F1"] = tm_f1
+#     metrics["REC"] = tm_rec
+#     metrics["PRE"] = tm_pre
+#     return metrics
 
 
 def absa_metrics_fn(list_hyp, list_label):
