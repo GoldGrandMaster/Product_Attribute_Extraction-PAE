14ade3898e7b43966e9f6afb8e9f00afb57f6570
passioneffort <agronkercishta.ai@gmail.com>
2023-10-26 14:17:09 -0400

    modify

--- modules/metrics.py
+++ modules/metrics.py
@@ -55,13 +55,13 @@ def entailment_metrics_fn(list_hyp, list_label):
     return metrics
 
 
-def document_sentiment_metrics_fn(list_hyp, list_label):
-    metrics = {}
-    metrics["ACC"] = accuracy_score(list_label, list_hyp)
-    metrics["F1"] = f1_score(list_label, list_hyp, average='macro')
-    metrics["REC"] = recall_score(list_label, list_hyp, average='macro')
-    metrics["PRE"] = precision_score(list_label, list_hyp, average='macro')
-    return metrics
+# def document_sentiment_metrics_fn(list_hyp, list_label):
+#     metrics = {}
+#     metrics["ACC"] = accuracy_score(list_label, list_hyp)
+#     metrics["F1"] = f1_score(list_label, list_hyp, average='macro')
+#     metrics["REC"] = recall_score(list_label, list_hyp, average='macro')
+#     metrics["PRE"] = precision_score(list_label, list_hyp, average='macro')
+#     return metrics
 
 
 def keyword_extraction_metrics_fn(list_hyp, list_label):
