94f78c75b13f32cfb4079a3dee0b14d8604dc989
passioneffort <agronkercishta.ai@gmail.com>
2023-11-06 02:49:18 -0500

    modify

--- modules/metrics.py
+++ modules/metrics.py
@@ -24,15 +24,15 @@ def aspect_extraction_metrics_fn(list_hyp, list_label):
     return metrics
 
 
-# def ner_metrics_fn(list_hyp, list_label):
-#     metrics = {}
-#     acc, pre, rec, f1, tm_pre, tm_rec, tm_f1 = conll_evaluation(
-#         list_hyp, list_label)
-#     metrics["ACC"] = acc
-#     metrics["F1"] = tm_f1
-#     metrics["REC"] = tm_rec
-#     metrics["PRE"] = tm_pre
-#     return metrics
+def ner_metrics_fn(list_hyp, list_label):
+    metrics = {}
+    acc, pre, rec, f1, tm_pre, tm_rec, tm_f1 = conll_evaluation(
+        list_hyp, list_label)
+    metrics["ACC"] = acc
+    metrics["F1"] = tm_f1
+    metrics["REC"] = tm_rec
+    metrics["PRE"] = tm_pre
+    return metrics
 
 
 def pos_tag_metrics_fn(list_hyp, list_label):
